be199c882672f57d8fdfd4178f8dd425bbb3cf03
Tesafilm1337 <koenig.simon1337@web.de>
2020-11-22 16:55:20 +0100

    Store things

--- Frontend/ess/src/app/app.module.ts
+++ Frontend/ess/src/app/app.module.ts
@@ -10,6 +10,9 @@ import { FormsModule } from '@angular/forms';
 import { LandingComponent } from './landing/landing.component';
 import { AuthenticationService } from './serivces/authentication.service';
 import { SignupComponent } from './auth/signup.component';
+// import { UsernameValidatorDirective } from './directives/username-validator.directive';
+// import { PasswordValidatorDirective } from './directives/password-validator.directive';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -18,12 +21,15 @@ import { SignupComponent } from './auth/signup.component';
     LoginComponent,
     LandingComponent,
     SignupComponent,
+    // UsernameValidatorDirective,
+    // PasswordValidatorDirective,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     BrowserAnimationsModule,
     AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [AuthenticationService],
   bootstrap: [AppComponent]
